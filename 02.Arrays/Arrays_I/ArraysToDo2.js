779ec0ab39d7c48ada93e3c82b78931b177b16cb
mdejac <dejacm@gmail.com>
2023-08-12 11:26:35 -0400

    ArraysToDo2 update for loop

--- 02.Arrays/Arrays_I/ArraysToDo2.js
+++ 02.Arrays/Arrays_I/ArraysToDo2.js
@@ -42,7 +42,7 @@ const shiftRight = arr => {
 
 const shiftLeft = arr => {
     let temp = arr[0];
-    for (let i=0; i<arr.length; i++) {
+    for (let i=0; i<arr.length-1; i++) {
         arr[i] = arr[i+1];
     }
     arr[arr.length-1] = temp;
